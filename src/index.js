657d0e3d2d84a2d6315f929aad175d278eeba5d7
Zoriana-Burian <ke7sha1991@gmail.com>
2020-10-21 13:39:22 +0300

    внесені правки та додано перемикання теми

--- src/index.js
+++ src/index.js
@@ -10,13 +10,49 @@ const Theme = {
 const refs = {
   body: document.querySelector('body'),
   menu: document.querySelector('.js-menu'),
-  switch: document.querySelector('theme-switch__toggle')
+  switch: document.querySelector('.theme-switch__toggle')
 };
 
 const menuMarkup = createMenu(menuEl);
 
 refs.menu.insertAdjacentHTML('afterbegin', menuMarkup);
 
+
 function createMenu(item) {
   return item.map(menuTemplates).join('');
 }
+
+
+
+
+refs.switch.addEventListener('change', onSetTheme);
+
+function onSetTheme(evt){
+
+   if(evt.target.checked) {
+    refs.body.classList.add(Theme.DARK);
+    refs.body.classList.remove(Theme.LIGHT); 
+    
+    localStorage.setItem('theme', Theme.DARK)
+   } else {
+    refs.body.classList.add(Theme.LIGHT);
+    refs.body.classList.remove(Theme.DARK);
+    
+    localStorage.setItem('theme', Theme.LIGHT)
+  } 
+
+}
+
+getLocalStorage()
+
+function getLocalStorage() {
+  const themeLocSt = localStorage.getItem('theme');
+  if (themeLocSt === Theme.DARK) {
+    refs.body.classList.add(Theme.DARK);
+    refs.switch.checked = true;
+  }
+}
+
+
+
+
